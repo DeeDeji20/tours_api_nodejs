3323ab480e5d7e96f94cd20a159b5fbd963c12b8
Adeola Oladeji <deolaoladeji@gmail.com>
2022-06-26 02:02:22 +0100

    Implementing tour api using nodeJS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const fs = require('fs')
 
 const app = express()
+app.use(express.json())
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/data/tours_simple.json`)
@@ -11,10 +12,17 @@ app.get("/api/v1/tours",(req, res)=>{
     console.log("I am running now");
     res.status(200).json({
             status:"success",
+            result:tours.length,
             data: {
                 tours: tours
             }
     })
+})
+
+app.post("/api/v1/tours", (req, res)=>{
+    console.log("Tired");
+    console.log(req.body);
+    res.send("Done")  
 })
 
 const PORT = 3000;
